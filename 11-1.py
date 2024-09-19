5400bb22cac9e11ab64a5618bc82809a0db66526
happyandy1121 <andymao20021121@gmail.com>
2024-09-19 13:46:51 +0800

    修正錯誤

---
+++ 11-1.py
@@ -0,0 +1,28 @@
+a = "content"
+print(f"len: {len(a)}")
+
+b = -55
+print(f"abs: {abs(b)}")
+
+c = 88
+print(f"str: {str(c)}")
+
+d = "99"
+print(f"int: {int(d)}")
+
+e = True
+print(f"type: {type(e)}")
+
+f = input("這邊填問題")
+print(f"input: {f}")
+
+g = "Today is Thursday"
+G = g.split(" ")
+print(f"split: {G}")
+
+h = [1]
+h.append("hello")
+h.append(3.14159)
+h.append(True)
+h.append([1, 2, 3])
+print(h)
