de89d11f7b27cd52d0675be3cc5f0529a3137174
happyandy1121 <andymao20021121@gmail.com>
2024-09-19 14:08:46 +0800

    簡報第12頁

---
+++ 11-2.py
@@ -0,0 +1,14 @@
+a = 40
+b = 3
+print(f"+: {a+b}")
+print(f"-: {a-b}")
+print(f"*: {a*b}")
+print(f"/: {a/b}")
+print(f"%: {a%b}")
+print(f"**: {a**b}")
+print(f"//: {a//b}")
+
+c = 1
+d = 0
+print(f"and: {c and d}")
+print(f"or: {c or d}")
